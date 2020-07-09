54cd6646084228c012854080982665261773b92c
olawolu <omg.tosine@gmail.com>
2020-07-09 11:44:32 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
+
+// Context keys:
+// Since setting a value in a context object requires the use of a key,
+// and while the value argument in a context object is of type interface{},
+// the key is also of type interface{}. This means we are not restricted to using only strings as the key,
+// which is good news when you consider how disparate code might well attempt to
+// set values with the same name in the same context, which would create problems.
+
+// We create a simple (private) struct for our keys and a helper method in order to get the value out.
+type contectKey struct {
+	name string
+}
+
+// Server is the API server
+type Server struct {
+	db *mgo.Session
+}
+
+// Key to store API key value in
+var contextKeyAPIKey = &contectKey{"api-key"}
+
+// APIKey is an helper funtion to extract the key, given a context
+func APIKey(ctx context.Context) (string, bool) {
+	key, ok := ctx.Value(contextKeyAPIKey).(string)
+	return key, ok
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+
+		// check if api key is valid
+		if !isValidAPIKey(key) {
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123ABC"
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
+
+func main() {}
