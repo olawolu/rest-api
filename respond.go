1e09e9570d92d3ce083aed00e3ada515a27fe29c
olawolu <omg.tosine@gmail.com>
2020-07-12 21:50:33 +0100

    added respondHTTPErr function to handle http specific errors

--- respond.go
+++ respond.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+// abstract the decoding and encodiing of data from and to the Request and ResponseWriter objects.
+// The decoder also closes the request body
+
 func decodeBody(r *http.Request, v interface{}) error {
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(v)
@@ -15,6 +18,7 @@ func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
+// Write the status code and some data to the response writer object
 func respond(w http.ResponseWriter, r *http.Request, status int, data interface{}) {
 	w.WriteHeader(status)
 	if data != nil {
@@ -22,10 +26,16 @@ func respond(w http.ResponseWriter, r *http.Request, status int, data interface{
 	}
 }
 
+// Handle errors related to responding
 func respondErr(w http.ResponseWriter, r *http.Request, status int, args ...interface{}) {
 	respond(w, r, status, map[string]interface{}{
 		"error": map[string]interface{}{
 			"message": fmt.Sprint(args...),
 		},
 	})
+}
+
+// HTTP-error specific helper to generate the correct message
+func respondHTTPErr(w http.ResponseWriter, r *http.Request, status int) {
+	respondErr(w, r, status, http.StatusText(status))
 }
