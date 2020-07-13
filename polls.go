6cb1fa202311258eaaa3b74c150568f0a4f697d6
olawolu <omg.tosine@gmail.com>
2020-07-13 08:48:16 +0100

    Implement function to read polls

--- polls.go
+++ polls.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -32,11 +33,42 @@ func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
 	respondHTTPErr(w, r, http.StatusNotFound)
 }
 
+// Reading polls
 func (s *Server) handlePollsGet(w http.ResponseWriter, r *http.Request) {
-	respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
+	var q *mgo.Query
+	var result []*poll
+
+	// Create copy of the database connection
+	session := s.db.Copy()
+	defer session.Close()
+
+	// create an object referring to the polls collection
+	c := session.DB("ballots").C("polls")
+	p := NewPath(r.URL.Path)
+
+	// build an mgo.Query object by parsing the path
+	if p.HasID() {
+		// get specific poll
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		// get all polls
+		q = c.Find(nil)
+	}
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
+		return
+	}
+	respond(w, r, http.StatusOK, &result)
 }
 
 func (s *Server) handlePollsPost(w http.ResponseWriter, r *http.Request) {
+
+	// create a copy of the database connection
+	session := s.db.Copy()
+	defer session.Close()
+
+	// create object refer
+
 	respondErr(w, r, http.StatusInternalServerError, errors.New("not implemented"))
 }
 
